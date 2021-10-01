c06961d12d8f83636fbf9dcd3cf4f081482dd694
nirajacharyaa <54741119+nirajacharyaa@users.noreply.github.com>
2021-10-01 14:50:20 +0545

    Added comments to the Maximum Subarray solution

--- Leetcode/MaximumSubarray/solution.java
+++ Leetcode/MaximumSubarray/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
+	// variable decleration and given
         int l=nums.length;
         int i=0,s=0,max=nums[0];
         int[] b = new int[l];
-        
+
+        // for all negative elements
         while(i!=l)
         {
             b[i]=nums[i];
@@ -15,15 +17,20 @@ class Solution {
         Arrays.sort(b);
         if(b[l-1]<=0)
             return b[l-1];
+	/* If last element after sorting is less than 0 every element is negative*/
     
+	// for atleast one non-negative element 
         i=0;
+	// finding the sum
         while(i!=l)
         {
             s+=nums[i];
+	// if sum is less than 0 initialize sum to 0
             if(s<0)
             {
                 s=0;
             }
+	// returning the max sum
             else if(s>max)
                 max=s;
             System.out.println(max);
