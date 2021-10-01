588d8bbefe8264fda3600dafe3e9c40818ab8eb1
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2021-10-01 13:19:41 +0530

    Added comments in solution.java

--- Leetcode/Jump Game/Solution.java
+++ Leetcode/Jump Game/Solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length==1)
-            return true;
-        int dist = 0;
+            return true; //If there is only one element, then it is possible to jump to it.
+        int dist = 0; 
         for(int i = 0; i < nums.length; i++){
-            dist=Math.max(dist, nums[i]);
+            dist=Math.max(dist, nums[i]); //Find the farthest distance that can be reached from the current position.
             if(dist==0 && i!=nums.length-1)
-                return false;
-            dist--;
+                return false; //If the farthest distance is 0, then it is impossible to reach the end.
+            dist--; //Reduce the farthest distance by 1.
         }
-        return true;
+        return true; //If the farthest distance is not 0, then it is possible to reach the end.
     }
 }
