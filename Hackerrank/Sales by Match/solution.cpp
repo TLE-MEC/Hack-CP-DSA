3e2c703d2f54eacab0ba3d7458e3c077d29c992c
Dj9162 <89787669+Dj9162@users.noreply.github.com>
2021-10-01 13:06:26 +0530

    Update solution.cpp

--- Hackerrank/Sales by Match/solution.cpp
+++ Hackerrank/Sales by Match/solution.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Function to calculate total number of pairs
 int sockMerchant(int n, vector<int> ar) {
-    int arr[1000] = {0};
-    for( int i =0 ; i < n ; i++)
+    int arr[1000] = {0}; // Making new array to count number of same colors of socks
+    for( int i =0 ; i < n ; i++) // Loop to update each color of sock to 1
     {
         arr[ar[i]]++;
     }
     int count = 0;
     for(int i=0; i<1000; i++)
     {
-        if( arr[i] == 0)
+        if( arr[i] == 0) // There is no socks of a color then leave it
             continue;
         else
-            arr[i] = arr[i]/2;
+            arr[i] = arr[i]/2; // If there is socks of same color make pair of it
     }
-    for(int i=0; i<1000 ; i++)
+    for(int i=0; i<1000 ; i++) // To count total number of paired socks
     {
         count += arr[i];
     }
@@ -23,7 +24,7 @@ int sockMerchant(int n, vector<int> ar) {
 }
 int main()
 {
-    int n;
+    int n;  // To store array size
     cout << "Enter size of array: ";
     cin >> n;
     vector<int> arr;
