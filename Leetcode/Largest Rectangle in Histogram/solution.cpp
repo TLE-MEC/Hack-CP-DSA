ffb0eabc4adc968770dc137f3753036b93de126c
sparsh77 <sparsh@7classes.com>
2021-10-01 14:32:26 +0530

    leetcode binary tree maximum path sum and leetcode largest rectangle in histogram

--- Leetcode/Largest Rectangle in Histogram/solution.cpp
+++ Leetcode/Largest Rectangle in Histogram/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
 
+//function for calculating next smaller element to the left
     vector <int> nsel(vector<int> arr, int n){
         vector<int> nselIndex;
         stack<pair<int, int>> s;
-
         for(int i=0; i<n; i++){
             while(!s.empty() && s.top().first >= arr[i]){
                 s.pop();
@@ -18,13 +18,15 @@ public:
             s.push({arr[i], i});
         }
         return nselIndex;
+        //this function will return the next smaller element to the left for every element
     }
 
+//function for calculating next smaller element to the right
     vector <int> nser(vector<int> arr, int n){
         vector<int> nserIndex;
         stack<pair<int, int>> s;
 
-        for(int i=n-1; i>=0; i--){
+        for(int i=n-1; i>=0;  i--){
             while(!s.empty() && s.top().first >= arr[i]){
                 s.pop();
             }
@@ -38,21 +40,22 @@ public:
         }
         reverse(nserIndex.begin(), nserIndex.end());
         return nserIndex;
+                //this function will return the next smaller element to the left for every element
     }
 
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> nselIndex =  nsel(heights, n);
-        vector<int> nserIndex =  nser(heights, n);
+        vector<int> nselIndex =  nsel(heights, n); //fetching next smaller element to the left separately
+        vector<int> nserIndex =  nser(heights, n); //fetching next smaller element to the right separately
         vector<int> ans;
         int max = INT_MIN;
 
         for(int i=0; i<n; i++){
-            ans.push_back((nserIndex[i] - nselIndex[i] - 1) * heights[i]);
+            ans.push_back((nserIndex[i] - nselIndex[i] - 1) * heights[i]); //calculating and storing all the Rectangle Area's in this array
             if(max < ans[i]){
-                max = ans[i];
+                max = ans[i]; //storing the max Rectangle Area of all area's present in array
             }
         }
-        return max;
+        return max; //returning that maximum area
     }
 };
