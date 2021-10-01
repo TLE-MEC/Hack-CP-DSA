585fb92b7be093b0dfbf94ca07636d45b1a8e192
Karanjot Singh <thestrangequarks@protonmail.com>
2021-10-01 19:46:20 +0530

    Update Sherlock and Anagramssolution.cpp

--- Hackerrank/SherlockAnagram/Sherlock and Anagramssolution.cpp
+++ Hackerrank/SherlockAnagram/Sherlock and Anagramssolution.cpp
@@ -1,3 +1,6 @@
+// Two string are anagrams if and only if for every letter occurring in any of them the number of its occurrences is equal in both the strings.
+// the only allowed letters are lowercase English letters, from a to z , the alphabet size is constant and its size is 26 . 
+//  This allows us to assign a constant size signature to each of the substring of s.
 #include <bits/stdc++.h>
 #include <string>
 #include <algorithm>
@@ -10,10 +13,10 @@ int sherlockAndAnagrams(string input) {
     int length = input.size();
     map<string, int> m;
     m.clear();
-    
+    // iterate over all substrings of s.
     for(int i=0; i<length; i++){
         for(int j=1; j<length+1-i; j++){
-            string t = input.substr(i,j);
+            string t = input.substr(i,j);      
             sort(t.begin(), t.end());
             m[t]++;
             
@@ -21,10 +24,11 @@ int sherlockAndAnagrams(string input) {
     }
     int ans = 0;
     
-    for(map<string, int>:: iterator itr = m.begin(); itr !=m.end(); itr++)
+    for(map<string, int>:: iterator itr = m.begin(); itr !=m.end(); itr++).  // intialize the substring value
         ans += (long)(itr->second)*(itr->second-1)/2;
     return ans;
 }
+// The time complexity is O|s^3| since we iterate over all O|s^2| substrings of  and for each substring we compute its signature in O|s| time.
 
 int main()
 {
