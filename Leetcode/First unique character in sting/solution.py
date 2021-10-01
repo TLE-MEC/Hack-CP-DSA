69a001770897e455479856e00e57d96625e9c680
Pavithra Devi Murugesan <pmurugesan@sandvine.com>
2021-10-01 17:22:24 +0530

    added the comments

--- Leetcode/First unique character in sting/solution.py
+++ Leetcode/First unique character in sting/solution.py
@@ -1,3 +1,16 @@
+"""
+387. Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Step 1:
+    - To find the first unique character in a string, first we need to count of all character in that string.
+    - Here i used the Counter method to find the count of all the letters in the given string and stored it into a dictionary.
+Step 2:
+    - Iterate over the dict and find the first key wih value 1.
+Step 3:
+    - return the index of the key.
+Step 4:
+    - If no key has value as '1' then return -1.
+
+"""
 from collections import Counter
 class Solution:
     def firstUniqChar(self, s: str) -> int:
@@ -10,3 +23,4 @@ class Solution:
             if c==1:
                 return s.index(let)
         return -1
+Solution().firstUniqChar("loveleetcode")
