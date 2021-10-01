705d8bb2c85ec82fc81e1fc1f67693b9ee32e411
SAI SUMANTH KUMAR <saisumanthkumar95509@gmail.com>
2021-10-01 18:56:05 +0530

    Update Apple and Orange.py

--- Hackerrank/Apple and Orange/Apple and Orange.py
+++ Hackerrank/Apple and Orange/Apple and Orange.py
@@ -6,31 +6,27 @@ import random
 import re
 import sys
 
-#
-# Complete the 'countApplesAndOranges' function below.
-#
 # The function accepts following parameters:
-#  1. INTEGER s
-#  2. INTEGER t
-#  3. INTEGER a
-#  4. INTEGER b
+#  1. INTEGER s = start point
+#  2. INTEGER t = End point
+#  3. INTEGER a = location of apple tree
+#  4. INTEGER b = location of orange tree
 #  5. INTEGER_ARRAY apples
 #  6. INTEGER_ARRAY oranges
-#
 
 def bwt(s,t,a,arr):
     array = []
     btw = 0
     for i in arr:
-        array.append(a+i)
+        array.append(a+i) # appending the relative locations to array with respective global origin
     for i in array:
         if i >= s and i <= t :
-            btw += 1
+            btw += 1  # checks wheather the item is there or not
     return btw
     
 def countApplesAndOranges(s, t, a, b, apples, oranges):
-    print(bwt(s,t,a,apples))
-    print(bwt(s,t,b,oranges))
+    print(bwt(s,t,a,apples))   # return No.of apples in between trees
+    print(bwt(s,t,b,oranges))  # return No.of oranges in between trees
 
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
