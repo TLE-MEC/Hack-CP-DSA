48252119237b4473d271ded3b7624a630b02487c
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-10-01 13:44:09 +0530

    Update Solution.cpp

--- Leetcode/Flipping an image/Solution.cpp
+++ Leetcode/Flipping an image/Solution.cpp
@@ -1,3 +1,14 @@
+/*
+Image is stored in 2-D bit array 
+So to flip the array,we need to run a for loop over each row
+Then we need to reverse the whole vector, with the use of reverse
+then we need to change the bit 0 trasnforms to 1, 1 transforms to 0
+for this we use the XOR operator (^) with 1.
+A XOR operator returns zero is both are same, and 1 if both are different,
+*/
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution {
 public:
   
