3a4991b924c8b9a5b319633345cf7f85dec2c612
Saurabh Daware <saurabhdaware2001@gmail.com>
2021-10-01 21:22:10 +0530

    Added Python Solution

--- Leetcode/Search Insert Position/Solution.java
+++ Leetcode/Search Insert Position/Solution.java
@@ -1,36 +1,35 @@
-Class Solution{
-
-public int searchInsert(int[]nums,int target){
-
-        int min=0;
-        int max=nums.length-1;
-        int mid=(max+min)/2;
-
-        while(min<=max){
-
-
-        if(nums[mid]==target)return mid;
-
-        //if(nums[min] < target && nums[max] > target) return mid;
-
-        else if(min==max){
-
-        if(nums[mid]>target)return mid;
-        else return mid+1;
-
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+	
+        // determine if array has a target inside
+		
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i]==target){
+                return i; 
+            }
         }
-        else if(min==mid){
-        if(nums[mid]>target)return mid;
+        
+        //at this point target is not inside of an array then
+        // check if target is bigger that last elemenet of an array
+        // if the target is bigger then it should be placed in nums.length position
+		
+        if(target > nums[(nums.length-1)])
+                    return nums.length;
+        
+        else
+            // if target is not bigger than the last element of an array
+            // determine where it should be located by comparing each element
+            // is less than of target or not. If an element is bigger than the target
+            //then the target should be placed in the element's index
+			
+            for (int i = 0; i < nums.length; i++){
+            if(nums[i]<target){
+                continue; 
+            }else{
+                return i;
+            }
         }
-
-        if(nums[mid]<target)min=mid+1;
-        else if(nums[mid]>target)max=mid-1;
-
-        mid=(max+min)/2;
-
-        }
-
-        return 0;
-        }
-
+        
+        return -1;
+    }
 }
