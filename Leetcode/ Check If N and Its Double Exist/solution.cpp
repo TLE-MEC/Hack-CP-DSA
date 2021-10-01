f9195a2e6301ad88cf8d19e082b392d79debb4c5
Vatsal Verma <59110359+vatsal259@users.noreply.github.com>
2021-10-01 12:44:54 +0530

    Updated the solution.cpp
    I would like to commit this in the main.
    Added the comments
    Given an array arr of integers, check if there exists two integers N and M such that N is the double of M ( i.e. N = 2 * M).
    More formally check if there exists two indices i and j such that :
    i != j
    0 <= i, j < arr.length
    arr[i] == 2 * arr[j]
    
    Hey there!, Thanks for submitting a PR to this repo
    Please complete the following checklist for your PR to be approved. Mark the items by adding an x.
    Example: - [ ] Mark me as completed
    To mark: - [x] Mark me as completed

--- Leetcode/ Check If N and Its Double Exist/solution.cpp
+++ Leetcode/ Check If N and Its Double Exist/solution.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 class Solution {
 public:
-    bool checkIfExist(vector<int>& arr) {
-        for(int i=0;i<arr.size();i++)
+    bool checkIfExist(vector<int>& arr)  //Creating the function
+    {
+        for(int i=0;i<arr.size();i++) //Starting a loop of size equal to the size of array
         {
-            for(int j=0;j<arr.size();j++)
+            for(int j=0;j<arr.size();j++) //Checking the corresponding elements and further.
             {
-                if(i==j)
+                if(i==j) //Checking if equal to continue
                 {
                     continue;
                 }
-                if(2*arr[i]==arr[j])
+                if(2*arr[i]==arr[j]) //Checking for the existance of double
                 {
-                    return true;
+                    return true; //Returning TRUE
                 }
             }
         }
-        return false;
+        return false; //Returning FALSE
     }
 };
