ef7ceacb747859e4edcf0d51932988484ba2613f
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2021-10-01 17:22:44 +0530

    Adding comments

--- Codeforces/GameWithArray/Solution.cpp
+++ Codeforces/GameWithArray/Solution.cpp
@@ -3,99 +3,24 @@
 #define db double
 #define rep(i, t) for (ll i = 0; i < t; i++)
 using namespace std;
-long long prime(int n)
-{
-    if (n == 1)
-        return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-
-    for (ll i = 5; i * i <= n; i += 6)
-    {
-        if (n % i == 0 || n % (i + 2) == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
-template <typename T>
-void print_vector(vector<T> &vec)
-{
-    for (auto elem : vec)
-        cout << elem;
-
-    cout << endl;
-}
-template <typename T>
-void print_vector_rev(vector<T> &vec)
-{
-    for (auto it = vec.rbegin; it != vec.rend; it++)
-        cout << *it << endl;
-
-    cout << endl;
-}
-template <typename T>
-void sort_inc(vector<T> &vec)
-{
-    sort(vec.begin(), vec.end());
-}
-template <typename T>
-void sort_dec(vector<T> &vec)
-{
-    sort(vec.rbegin(), vec.rend());
-}
-template <typename T>
-void inp(T &a)
-{
-    cin >> a;
-}
-template <typename T>
-void inp(T &a, T &b)
-{
-    cin >> a >> b;
-}
-template <typename T>
-void inp(T &a, T &b, T &c)
-{
-    cin >> a >> b >> c;
-}
-template <typename T>
-void inp(T &a, T &b, T &c, T &d)
-{
-    cin >> a >> b >> c >> d;
-}
-template <typename T>
-void inp(T &a, T &b, T &c, T &d, T &e)
-{
-    cin >> a >> b >> c >> d >> e;
-}
-template <typename T>
-void inp(vector<T> &vec)
-{
-    for (int i = 0; i < vec.size(); i++)
-        cin >> vec[i];
-}
-void solve1()
+void solve1()  //Main function
 {
     ll n, s;
-    cin >> n >> s;
-    if (n == 1)
+    cin >> n >> s;  //Taking inputs
+    if (n == 1)  //if n=1 then length of requirred array is 1
     {
-        if (s == 1)
+        if (s == 1)  // if s=1 then no case possible
             cout << "NO" << endl;
         else
         {
-            cout << "YES" << endl;
-            cout << s << endl;
-            cout << s - 1 << endl;
+            cout << "YES" << endl;  
+            cout << s << endl; 
+            cout << s - 1 << endl;  // if s!=1 then k will be s and array element will be s-1
         }
     }
     else
     {
-        if (s <= n)
+        if (s <= n)  //no case possible when s<=n
         {
             cout << "NO" << endl;
         }
@@ -106,24 +31,25 @@ void solve1()
             bool cond = 0;
             for (int i = 0; i < (n - 1); i++)
             {
-                if ((fnum + i) == num||(s-(fnum+i))==num)
+                if ((fnum + i) == num||(s-(fnum+i))==num)  //checking for condition
                 {
                     cond = 1;
                     break;
                 }
             }
-            if (cond)
+            if (cond)  //if the condition statisfies then no answer possible
             {
-                cout << "NO" << endl;
+                cout << "NO" << endl; 
             }
             else
             {
+                //Else the array exists with the following solution
                 cout << "YES" << endl;
                 for (int i = 0; i < (n - 1); i++)
                 {
                     cout<<1<<" ";
                 }
-                cout<<fnum<<endl;
+                cout<<fnum<<endl;  
                 cout<<num<<endl;
             }
         }
@@ -138,11 +64,10 @@ int main()
     freopen("out.txt", "w", stdout);
 #endif
     ll t;
-    // cin>>t;
     t = 1;
     rep(i, t)
     {
-        solve1();
+        solve1(); //going to the funnction which solves the problem
     }
     return 0;
 }
