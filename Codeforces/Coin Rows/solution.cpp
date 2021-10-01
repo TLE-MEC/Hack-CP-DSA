0a60aa35b0377e26ce2d254fc6cddcc1558eff86
addy0110 <addycoder0110@gmail.com>
2021-10-01 16:11:26 +0530

    Update solution.cpp
    Added comments for better understanding

--- Codeforces/Coin Rows/solution.cpp
+++ Codeforces/Coin Rows/solution.cpp
@@ -7,18 +7,18 @@ int main(){
     while(cases--){
         int n;
         cin>>n;
-        vector <ll int> sum1(n+1,0);
-        vector <ll int> sum2(n+1,0);
+        vector <ll int> sum1(n+1,0); // Declaring vector of size n+1 and assigning 0 to all elements
+        vector <ll int> sum2(n+1,0); // Declaring vector of size n+1 and assigning 0 to all elements
         for(int i = 1 ; i < n+1 ; i++)
         cin>>sum1[i];
 
         for(int i = 1 ; i < n+1 ; i++)
         cin>>sum2[i];
 
-        for(int i = 1 ; i < n+1 ; i++)
+        for(int i = 1 ; i < n+1 ; i++) // Prefix Sum
         sum1[i] += sum1[i-1];
 
-        for(int i = n-1 ; i > 0 ; i--)
+        for(int i = n-1 ; i > 0 ; i--) // Suffix Sum
         sum2[i] += sum2[i+1];
 
         ll int ans = 10000000000,temp1,temp2,temp;
@@ -27,6 +27,8 @@ int main(){
             temp2 = sum2[1] - sum2[i];
             temp = max(temp1,temp2);
             ans = min(ans,temp);
+
+            // For every index finding the maximum answer we can get if we will move down from it.
         }
         cout<<ans<<endl;
     }
