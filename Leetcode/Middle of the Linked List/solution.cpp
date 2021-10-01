bc930eba45c1068aa53ee6c3941a2fea8d2b7f2c
Dhruv Bansal <dhruvbansal237@gmail.com>
2021-10-01 11:40:20 +0530

    Add comments explaining logic.

--- Leetcode/Middle of the Linked List/solution.cpp
+++ Leetcode/Middle of the Linked List/solution.cpp
@@ -8,15 +8,17 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//we will use Floyd's algorithm(also known as tortoise and hare algorithm) for solving this problem.
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *slow=head;
-        ListNode *fast=head;
+        ListNode *slow=head;//this will be the slow pointer which will move one node at a time
+        ListNode *fast=head;//this will be the fast pointer which will move two nodes at a time
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
+        //when the above loop terminates, the slow pointer will be at the desired middle node of the linked list. Hence we simply return the slow pointer.
         return slow;
     }
 };
