b3f338e99d5f98d3770d9cbd1c7dcda3986d1b05
Anthima Singh <58143009+Anthima@users.noreply.github.com>
2021-10-01 22:38:32 +0530

    Update rempve.py

--- Leetcode/Remove Duplicates from Sorted Array/rempve.py
+++ Leetcode/Remove Duplicates from Sorted Array/rempve.py
@@ -1,12 +1,20 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums)==0:
-               return 0
-            
-        j=0
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # If array is empty
+        if len(nums) == 0:
+            return 0
+        # Two Pointer Solution
+        # j is slow runner, i is fast runner
+        j = 0;
         for i in range(1, len(nums)):
-              if nums[i] != nums[j]:
-                j +=1
+        # If they are unique, change the array and increment both
+            if nums [i] != nums[j]:
+                j += 1
                 nums[j] = nums[i]
-        return j+1
-        
+        # Return the slow runner + 1 for length
+        return j + 1
