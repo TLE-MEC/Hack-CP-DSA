184ec3f567aedec89c85bfcc55305c25fe8b482e
sangeetamukherjee092@gmail.com <sangeetamukherjee092@gmail.com>
2021-10-01 20:14:13 +0530

    solved the hackerrank staircase question in c++

--- Hackerrank/Staircase/solution.cpp
+++ Hackerrank/Staircase/solution.cpp
@@ -1,21 +1,62 @@
-#include<iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main () {
-    int height;
-    cin >> height;
+string ltrim(const string &);
+string rtrim(const string &);
 
-    for (int i = 1; i <= height; i++) {
-        for (int j = 0; j < i; j++) {
-            if(j==0) {		
-                //Printing spaces 
-                for(int t = 0; t < height - i; t++) cout << " ";
-            }
-            //Print hashes
-            cout << "#";
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void staircase(int n) {
+    for(int i=1;i<=n;i++)
+    { int j;
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";    
         }
-        cout << endl;
+        for(int k=j;k<=n;k++)
+        {
+            cout<<"#";    
+        }
+        cout<<"\n";
     }
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    staircase(n);
+
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
