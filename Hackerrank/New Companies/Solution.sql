d4c4316d0b5599dd02f31589a8b5eb6ef23f15cc
aryabagde <akruyhau@gmail.com>
2021-10-02 01:26:47 +0530

    Link and Solution for the question New Companies SQL

---
+++ Hackerrank/New Companies/Solution.sql
@@ -0,0 +1,10 @@
+//Remember we can use alias even before defining them//
+SELECT c.company_code, c.founder, 
+       COUNT(DISTINCT l.lead_manager_code), COUNT(DISTINCT s.senior_manager_code),
+       COUNT(DISTINCT m.manager_code), COUNT(DISTINCT e.employee_code)
+FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e
+WHERE c.company_code = l.company_code AND 
+      l.lead_manager_code = s.lead_manager_code AND
+      s.senior_manager_code = m.senior_manager_code AND
+      m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder ORDER BY c.company_code;
