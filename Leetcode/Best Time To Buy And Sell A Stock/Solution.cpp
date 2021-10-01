77efb0b750692a8333fc0bcec914e1d8f73843e3
Mihir_Chhabria <67017533+MihirChhabria@users.noreply.github.com>
2021-10-01 22:51:46 +0530

    update solution.cpp
    Added comments to solution for easy-understanding.

--- Leetcode/Best Time To Buy And Sell A Stock/Solution.cpp
+++ Leetcode/Best Time To Buy And Sell A Stock/Solution.cpp
@@ -1,4 +1,6 @@
-//
+//Profit = Selling price - purchase price
+//With each loop, we compare prices[i] element with the previous minimum value and update accordingly
+//With each loop, we compute profit , if the value of profit is greater than the previous value, update profit to get max profit.
 
 
 class Solution {
@@ -8,7 +10,7 @@ public:
         int min_value=prices[0];
         for(int i=1;i<prices.size();i++){
            min_value=min(prices[i],min_value);
-            profit=max(profit,(prices[i]-min_value);
+            profit=max(profit,(prices[i]-min_value);//max profit
         }
         return profit;
     }
