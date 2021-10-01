2cf0a1540707152853677fad959e017a3f0cdd2d
SAI SUMANTH KUMAR <saisumanthkumar95509@gmail.com>
2021-10-01 19:01:05 +0530

    Update Grading Students.py

--- Hackerrank/Grading Students/Grading Students.py
+++ Hackerrank/Grading Students/Grading Students.py
@@ -17,12 +17,12 @@ def gradingStudents(grades):
         if i < 38:               # checking base case for pass and fail
             final_grades.append(i)
         else:
-            if grade_round(i) - i < 3:   # checking whether the increment is less than 3
+            if grade_round(i) - i < 3:              # checking whether the increment is less than 3
                 final_grades.append(grade_round(i)) # if it is less than 3 , then append the round value to final grades
             else:
-                final_grades.append(i) # if it is greater than 3 , then appends the actual value to final grades
+                final_grades.append(i)              # if it is greater than 3 , then appends the actual value to final grades
 
-    return final_grades
+    return final_grades                             # returns the final grades array
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
