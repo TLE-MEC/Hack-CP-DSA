242e3aa2a3b5c3c9259e62d16e116a3c60b062b8
sangeetamukherjee092@gmail.com <sangeetamukherjee092@gmail.com>
2021-10-01 22:58:01 +0530

    Solution to Staircase problem

---
+++ Hackerrank/Staircase/solution2.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+void staircase(int n) {
+    for(int i=1;i<=n;i++)
+    { int j;
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";   //for printing spaces 
+        }
+        for(int k=j;k<=n;k++)
+        {
+            cout<<"#";   //for printing #
+        }
+        cout<<endl;
+    }
+
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    staircase(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
