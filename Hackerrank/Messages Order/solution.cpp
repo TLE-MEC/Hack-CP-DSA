2851f1d1aa655e6f7652efb4ee491875135d346a
Aman Singh <68653906+Gagarod@users.noreply.github.com>
2021-10-02 21:05:05 +0530

    Update solution.cpp

--- Hackerrank/Messages Order/solution.cpp
+++ Hackerrank/Messages Order/solution.cpp
@@ -2,41 +2,43 @@
 #include <algorithm>
 #include <vector>
 
+//Editable code---------------------------------------------------------------------------------------------------------------------------
 using namespace std;
 
 class Message {
+    //private data members Text, id, current_id
     string Text;
     static int id;
     int current_id;
 public: 
-    Message() {
+    Message() { // Default Constructor
         Text="";
         current_id=++id;
     }
-    Message(string text){
+    Message(string text){ // Parameterised Constructor
         Text=text;
         current_id=++id;
     }
     const string& get_text() {
         return Text;
     }
-    bool operator < (const Message& M2) {
+    bool operator < (const Message& M2) { // Operator Overloading of <
         if(current_id < M2.current_id)
             return true;
         else
             return false;
     }
 };
-int Message::id=0;
+int Message::id=0; //Initialise static variable
 
 class MessageFactory {
 public:
     MessageFactory() {}
     Message create_message(const string& text) {
-        return Message(text);
+        return Message(text);// retur object of type Message
     }
 };
-
+//---------------------------------------------------------------------------------------------------------------------------
 class Recipient {
 public:
     Recipient() {}
