ce2127684395c96f4f963c04ae02b1a0a5876855
jahgath <jagathjijo01@gmail.com>
2021-10-02 15:39:10 +0530

    Added comments

--- Leetcode/Reverse Linkedlist/solution.cpp
+++ Leetcode/Reverse Linkedlist/solution.cpp
@@ -5,12 +5,21 @@ public:
         ListNode *dummy_head = new ListNode(0);
         dummy_head->next = head;
         
-        ListNode *prev = nullptr;
+        //intialise previous node pointing to null
+        ListNode *prev = nullptr; 
+
+        //run while loop till end of list
         while(head!=nullptr){
+            //initialise next node pointing to the node after the current node
             ListNode *next_node = head->next;
+
+            //eg: null->1->2
+            
             head->next = prev;
             prev = head;
             head = next_node;
+
+            //2->1->null
         }
         
         return prev;
