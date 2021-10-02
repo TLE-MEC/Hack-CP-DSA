5317b4e10e8b72dd39bf23a0e64ac953a029af00
Jhoser Pacheco <jhoserpacheco@gmail.com>
2021-10-02 11:49:50 -0500

    add comments solution

--- URIOnlineJudge/Carnaval/solution.java
+++ URIOnlineJudge/Carnaval/solution.java
@@ -20,13 +20,16 @@ public class Main
 	Scanner sc = new Scanner (System.in);
 	float vec[] = new float[5];
 	int n = 5;
+	// read numbers in array 
 	  for (int j = 0; j < n; j++) {
                 vec[j] = sc.nextFloat();
             }
+			//sort array 
             Arrays.sort(vec);
-            
+            //Here you take the [1,3] highest grades you had, ignoring the first and last 
             float xd = vec[1]+vec[2]+vec[3];
+			//print 
             System.out.println(String.format("%.1f",xd));
 
 	}
-}
+}//finish
