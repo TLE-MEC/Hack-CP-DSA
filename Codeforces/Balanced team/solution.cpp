6f818013f30f9977fa2672625b83fd0979661e08
Progya Das <78193576+progyadas@users.noreply.github.com>
2021-10-03 00:19:27 +0530

    Added comments

--- Codeforces/Balanced team/solution.cpp
+++ Codeforces/Balanced team/solution.cpp
@@ -6,15 +6,15 @@ int32_t main(){
     cin>>n;
     int ar[n];
     for(int i=0;i<n;i++){
-        cin>>ar[i];
+        cin>>ar[i]; //to input the programming skill of the ith student
     }
-    sort(ar,ar+n);
-    int ans=0;
+    sort(ar,ar+n); //sorting the array of programming skills in ascending order
+    int ans=0; //to store the maximum possible number of students in balanced team
 	int j=0;
 	for (int i=0;i<n;++i) {
-		while (j<n&&ar[j]-ar[i]<=5) {
+		while (j<n&&ar[j]-ar[i]<=5) { //to find the continuous number of students whose difference in programming skill is less than or equal to 5
 			j++;
-			ans=max(ans,j-i);
+			ans=max(ans,j-i); //storing the maximum possible value
 		}
 	}
     cout<<ans<<endl;
