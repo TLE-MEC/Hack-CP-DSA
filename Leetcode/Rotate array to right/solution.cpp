8bd1646fee825ec1a7de3c488d61121c3aa6ae10
Harivams Sai Gudalwar <88828522+harivams-sai@users.noreply.github.com>
2021-10-02 14:08:18 +0530

    Update solution.cpp

--- Leetcode/Rotate array to right/solution.cpp
+++ Leetcode/Rotate array to right/solution.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// A driver function to reverse an array/vector from index x to index y using swap(x,y)
 void reverse(vector<int>& nums, long long int x, long long int y) {
 	for (long long int i = x, j = y; i < j; i++, j--) {
 		swap(nums[i], nums[j]);
@@ -13,11 +14,11 @@ int main() {
 	int k = 3;			//nums = [1,2,3,4,5,6,7], k = 3 (example testcase)
 	int n = nums.size();
 	k = k % n;
-	reverse(nums, n - k, n - 1);
-	reverse(nums, 0, n - k - 1);
-	reverse(nums, 0, n - 1);
+	reverse(nums, n - k, n - 1);   //reverse array from n-k to n-1 index
+	reverse(nums, 0, n - k - 1);   //reverse remaining elements from 0 to n-k-1 indices
+	reverse(nums, 0, n - 1);       //finally reverse whole array from 0 to n-1 indices
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < n; ++i)    // print whole array/vector
 	{
 		cout << nums[i] << " ";
 	}
