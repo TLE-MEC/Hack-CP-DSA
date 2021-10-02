f95ba7210a3acc77e1396c2a567d495f08978bce
Rakshit Gupta <75110126+RakshitGupta621@users.noreply.github.com>
2021-10-02 08:01:36 +0530

    Update solution.cpp

--- Leetcode/Increasing Triplet Subsequence/solution.cpp
+++ Leetcode/Increasing Triplet Subsequence/solution.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
+    //Sample input nums = [1,2,3,4,5]
     bool increasingTriplet(vector<int>& nums) {
         // O(n) time and O(1) space
+        
+        // The below 3 lines are just used for to make the code faster
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);   
         cout.tie(NULL);
@@ -11,12 +14,20 @@ public:
         for (int &i : nums){
             if (i < min1) 
                 min1 = i;
+            // check if nums[i]<min1, if yes then update min1 to nums[i]
+            // 1 < INT_MAX, set min1 = 1
+            
             else if (i > min1 && i < min2) 
                 min2 = i;
+            // check if nums[i]>min1 and nums[i]<min2, if yes then update min2 to nums[i]
+            // as 2 > 1 and 2 < INT_MAX, set min2 = 2 
+            
             else if (i > min2) 
                 return true;
+            // as 3 > 2 now we fulfill the condition of increasing triplet subsequence and return true
         }
         return false;
+        // otherwise return false
     }
 };
 
