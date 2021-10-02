934c0d79067041831765b487964afa536cdaf547
Aman Singh <68653906+Gagarod@users.noreply.github.com>
2021-10-02 20:15:42 +0530

    Update solution.java

--- Leetcode/Sort colors/solution.java
+++ Leetcode/Sort colors/solution.java
@@ -1,3 +1,14 @@
+//We partition the given array into three parts
+//Make 3 variables zero and second
+//zero=-1 whereas second=nums.length
+//run for loop from i=0 till i<second
+//If nums[i]==2 && i<second
+//      decrease second by one
+//      swap nums[i] with nums[second] as all 2s must be placed at last
+//If nums[i]==0 && i>zero
+//      increase zero by one
+//      swap nums[i] with nums[zero] as all 0s must be placed at the start
+
 class Solution {
     public static void sortColors(int[] nums) {
         int second=nums.length, zero=-1;
