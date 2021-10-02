e6b6fa7c41422c0207b948b2b8f57a704f5eec28
Yashi Shukla <59660649+yashi4001@users.noreply.github.com>
2021-10-02 17:41:09 +0530

    Added Preorder to Postorder GFG solution

---
+++ GeeksForGeeks/Preorder to Postorder/Solution.java
@@ -0,0 +1,23 @@
+class Solution{
+    //Function that constructs BST from its preorder traversal.
+    public static Node post_order(int pre[], int size) 
+    {
+        int rootVal=pre[0];
+        Node root=new Node(rootVal);
+        for(int i=1;i<pre.length;i++){
+            root=insert(pre[i],root);
+        }
+        return root;
+    } 
+    public static Node insert(int val,Node root){ //function for inserting nodes 
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        else if(val<root.data)
+        root.left=insert(val,root.left);
+        else
+        root.right=insert(val,root.right);
+        return root;
+    }
+}
