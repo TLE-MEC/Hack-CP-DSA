a113185e54b30736e0c25c5e527638272901edbb
AkhileshManda <70640191+AkhileshManda@users.noreply.github.com>
2021-10-02 08:34:27 +0530

    Added comments

--- Codechef/Travel Pass/solution.py
+++ Codechef/Travel Pass/solution.py
@@ -1,10 +1,18 @@
 for _ in range(int(input())):
-    N,A,B = map(int, input().split())
+    N,A,B = map(int, input().split()) 
+    # N => length of string
+    # A => Inter-district time
+    # B => Inter-state time
     s=input()
     time=0
+    
+    #for every element in s
     for i in s:
+        #if i is 1 then inter-state time added
         if(i=="1"):
             time+=B
+        #if i is 0 then inter-district time added 
         if(i=="0"):
             time+=A
+    #output
     print(time)      
