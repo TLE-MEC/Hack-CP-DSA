f07cf4194804f540bdf6c6a3023de125e9a294e7
narotam26 <nsv261102@gmail.com>
2021-10-02 20:54:26 +0530

    changes made

--- GeeksForGeeks/Sort an array of 0s, 1s and 2s/Solution.cpp
+++ GeeksForGeeks/Sort an array of 0s, 1s and 2s/Solution.cpp
@@ -1,18 +1,27 @@
+//Concept:- Dutch National Flag problem
+
+//We will make 3 pointers low, mid and high
+//low = mid = array[0] and high = array[n-1]
+//Iterate till (mid<=high)
+//If array[mid] = 0 then swap array[low] with array[mid] and increment low and mid by 1 i.e (low=low+1 and mid=mid+1)
+//if array[mid] = 1 just increment mid by 1 i.e (mid=mid+1)
+//if array[mid] = 2 then swap a[mid] with a[high] and Decrement high by 1 i.e (high=high-1)
+
 // Time complexity :- O(n)
 // As the array is traversed only once therefore, the time complexity of the algorithm is O(n).
 
 // Space complexity :- O(1)
 // As no extra space is used therefore, the space complexity of the algorithm is O(1).
 
+
 #include<iostream>
 using namespace std;
 
-
 void DNFsort(int array[], int n) // n is the size of the array  
 {  
-    int low = 0;    
-    int mid = 0;  
-    int high = n - 1;
+    int low = 0;         //first element of array
+    int mid = 0;         //first element of array
+    int high = n - 1;    //last element of array
       
       //Iterate till (mid<=high)
 
@@ -23,17 +32,17 @@ void DNFsort(int array[], int n) // n is the size of the array
               
         case 0:  
             swap(array[low], array[mid]);
-            low++;
-            mid++;  
+            low++;      // incrementing low by 1
+            mid++;      // incrementing mid by 1
             break;  
                 
         case 1:  
-            mid++;  
+            mid++;      // incrementing mid by 1
             break;  
                
         case 2:  
             swap(array[mid], array[high]);
-            high--;  
+            high--;     // decrementing high by 1
             break;  
         }  
     }  
@@ -45,16 +54,17 @@ int main()
     cout<<"Enter size of array:";
     cin>>n;
 
-    int array[n];
+    int array[n];           //array of n elements containing only 0s , 1s and 2s
     
     cout<<"Enter elements :";
     for(int i=0;i<n;i++)
     cin>>array[i];
 
-    DNFsort(array,n);
+    DNFsort(array,n);       //calling the sorting function to sort the array
+
     cout<<"Sorted Array:\n";
     for(int i=0;i<n;i++)
-    cout<<array[i]<<" ";
+    cout<<array[i]<<" ";    //printing the sorted array
     
     return 0;
 
