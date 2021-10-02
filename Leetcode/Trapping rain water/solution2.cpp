9700b606cdc4927e6a3f72b049b67bac960fd8aa
aishaandatt <63548745+aishaandatt@users.noreply.github.com>
2021-10-02 14:04:59 +0530

    Added Comments

--- Leetcode/Trapping rain water/solution2.cpp
+++ Leetcode/Trapping rain water/solution2.cpp
@@ -10,17 +10,17 @@ public:
             int righti = i;
             while(lefti >= 0)
             {
-                maxL = max(maxL,height[lefti]);
+                maxL = max(maxL,height[lefti]);  //to find the maximum height from left side
                 lefti--;
             }
              while(righti < height.size())
             {
-                maxR = max(maxR,height[righti]);
+                maxR = max(maxR,height[righti]); //to find the maximum height from left side
                 righti++;
             }
-            int current = min(maxR,maxL) - height[i];
+            int current = min(maxR,maxL) - height[i];  //finding the amount of water it can store currently
             if(current >= 0)
-                total += current;
+                total += current; //adding to total to get final result
         }
         return total;
     }
