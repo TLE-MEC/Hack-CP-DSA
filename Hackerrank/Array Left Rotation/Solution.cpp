b5a017926012a19f66950e80869bad8f3b5fe5ef
Uzzal Saha <55195197+uzzal623@users.noreply.github.com>
2021-10-02 23:18:00 +0600

    Update Solution.cpp
    added comment

--- Hackerrank/Array Left Rotation/Solution.cpp
+++ Hackerrank/Array Left Rotation/Solution.cpp
@@ -7,15 +7,17 @@ using namespace std;
 
 
 int main() {
-    int N, d; cin >> N >> d;
-    vector<int> v(N);
-    for (size_t i = 0; i < v.size(); ++i) {
-        cin >> v[i];
+    int N, d; // N = array , d = operation we have to perform
+    cin >> N >> d; //taking input of n and d
+    vector<int> v(N); // specifing that the vector has a size of N
+    for (size_t i = 0; i < v.size(); ++i) 
+    {
+        cin >> v[i]; //taking input
     }
     d = d % N;
     for (int i = d; i < N; ++i)
-        cout << v[i] << ' ';
-    for (int i = 0; i < d; ++i)
-        cout << v[i] << ' ';
+        cout << v[i] << ' '; // printing the array output
+    for (int i = 0; i < d; ++i) // for loop
+        cout << v[i] << ' '; // printing the array output
     return 0;
 }
