cb375c11e964bd5d9f22eef3b54f74885aa182fb
shreya <shreyaanand2908@gmail.com>
2021-10-02 15:19:34 +0530

    Added Leetcode XOR Queries of a Subarray

--- Leetcode/XOR Queries of a Subarray/solution.cpp
+++ Leetcode/XOR Queries of a Subarray/solution.cpp
@@ -1,3 +1,16 @@
+//In this probelem, we are given Given an array arr of positive integers and the array queries 
+//where queries[i] = [Li, Ri], for each query i, we have to compute the XOR of elements from Li to Ri 
+//(that is, arr[Li] xor arr[Li+1] xor ... xor arr[Ri] ). 
+// Finally, Return an array containing the result for the given queries.
+
+// Solution-
+// The logic is using the property of xor according to which, a^a=0 and 0^a=0 => a^b^a=b
+// 1. Calculate a prefix xor, at its ith position, it holds the xor for all the elements of the array from 0 to i
+// 2. while iterating through the queries array, calculate the xor of the numbers at (li-1)th position
+// and (ri)th position of the pre_xor array
+// 3. This will give the ans of the xor of elements between li and ri since the repeated elements will yield 0 upon xor
+// Time Complexity - O(n) in worst case 
+
 #include<bits/stdc++.h>
 using namespace std;
 
