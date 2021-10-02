69de309f4cdfc8ef68a3c0d31a2d7056e5812231
Nihal Ahmad <nehalhmd14@gmail.com>
2021-10-02 17:45:15 +0530

    made necessary comments

--- Leetcode/Majority Element/solution.cpp
+++ Leetcode/Majority Element/solution.cpp
@@ -1,3 +1,6 @@
+// ##Approach: Since we know that the majority element will be present for count > size/2 times , therefore if we try an approach to cancel dissimilar elements with each other, then at last there will be only majority element left because its count is more than size/2 and there will be not much elements to fully cancel it.
+// So, whenever we get count=, we update our elem to be that array element and if now any array element = elem then, we decrement the count. Like this we cancel each and every element except majority. and at last majority element will be left and stored in elem variable.
+
 // ##TimeComplexity: Since we travel vector only once, thus time taken will be O(N).
 
 // ##SpaceComplexity: Since no extra data structure is used, so space Complexity will be O(1). 
