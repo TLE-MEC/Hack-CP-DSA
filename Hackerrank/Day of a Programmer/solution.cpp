31410703b352e3827f0130f16c0f2927aa3f7a2e
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2021-10-02 11:51:10 +0530

    Update solution.cpp

--- Hackerrank/Day of a Programmer/solution.cpp
+++ Hackerrank/Day of a Programmer/solution.cpp
@@ -3,13 +3,13 @@ using namespace std;
 typedef long long ll;
 int main()
 {
-    ll y;
+    ll y; //variable for year 
     cin >> y;
-    if (y == 1918)
+    if (y == 1918) //Edge case for transition from russian to georgian calender
         cout << "26.09." << y;
     else if (y <= 1917)
     {
-        if (y % 4 == 0)
+        if (y % 4 == 0) //checking leap year according to russian calender
             cout << "12.09.";
         else
             cout << "13.09.";
@@ -17,7 +17,7 @@ int main()
     }
     else
     {
-        if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))
+        if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0)) // checking leap year according to the georgian calender
             cout << "12.09.";
         else
             cout << "13.09.";
