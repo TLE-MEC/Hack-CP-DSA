31a6c11b013f22f08778fa539c037821f2ffba77
AkhileshManda <70640191+AkhileshManda@users.noreply.github.com>
2021-10-02 08:36:59 +0530

    Added comments

--- Hackerrank/polar coordinates/solution.py
+++ Hackerrank/polar coordinates/solution.py
@@ -1,4 +1,6 @@
+#code to implement few methods of cmath module 
 import cmath
 num=complex(input())
-print(abs(num))
-print(cmath.phase(num))
+#takes input of the form a +ib where a,b are any real numbers
+print(abs(num)) #abs(num) returns absolute value or mod of complex number (a^2+b^2)^0.5
+print(cmath.phase(num)) #returns phase in complex plane i.e arctan(b/a)
