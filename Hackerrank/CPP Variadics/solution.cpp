1adac2eb1251934163637ae83f09537b9798f856
Aman Singh <68653906+Gagarod@users.noreply.github.com>
2021-10-02 21:53:11 +0530

    added commits

--- Hackerrank/CPP Variadics/solution.cpp
+++ Hackerrank/CPP Variadics/solution.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
+
 //Editable code--------------------------------------------------------------------------------------
+
+// A template function to implement reversed_binary_value.
 template<bool... digits> 
 int reversed_binary_value(){
     int sum = 0;
     int product = 1;
     bool binary[] = { digits... }; //array of type bool with 0 or more elements
     for (bool digit : binary) {   // convert bit array into integer number(reversed)
-        sum += (digit ? product : 0);
+        sum += (digit ? product : 0); // if digit has value true/1 add product to sum or add 0 to it
         product *= 2;
     }
-    return sum; // return number
+    return sum; // return sum
 };
+
 //----------------------------------------------------------------------------------------------------
 
 template <int n, bool...digits>
