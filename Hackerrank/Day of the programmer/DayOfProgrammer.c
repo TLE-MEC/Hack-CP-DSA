0b7f6e7657b735cea85e6d39b1cb1cd65bff6c2f
BAKCHODGUNDA <aplinkinpark16@gmail.com>
2021-10-02 10:42:33 +0530

    DayofProgrammer solution in C

--- Hackerrank/Day of the programmer/DayOfProgrammer.c
+++ Hackerrank/Day of the programmer/DayOfProgrammer.c
@@ -1,24 +1,24 @@
-//Solution by Akshat Pandey
+//DayOfProgrammer Solution by Akshat Pandey
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int year,leap=0; 
+    int year,leap=0;               //initializing year and leap variable
     scanf("%d",&year);
-    if (year%4==0) 
+    if (year%4==0)                 //checking if year is leap or not
           leap=1;
-    if (year>1918){
-    if (year % 100==0) 
+    if (year>1918){                
+    if (year % 100==0)             //an year divided by 100 is not leap year
           leap=0;
-    if (year % 400==0) 
+    if (year % 400==0)             //an year divided by 400 is leap year
           leap=1;
     }
     if (year!=1918){
-        if(leap==0)
-               printf("13.09.%d\n",year);
+        if(leap==0)           
+               printf("13.09.%d\n",year);         
         else 
-               printf("12.09.%d\n",year);
+               printf("12.09.%d\n",year);         
     }
     else 
           printf("26.09.%d\n",year);
