bcc1c180dbb77cf1d2123b7c9ca5733160492f10
Iftekhar Ahmed <iftekharifat007@gmail.com>
2021-10-02 15:36:52 +0600

    added leetcode problem Shuffle the Array (Easy)

--- Leetcode/Shuffle the Array/Solution.java
+++ Leetcode/Shuffle the Array/Solution.java
@@ -6,8 +6,8 @@ class Solution {
         int remInd = 0;
 
         for(int i=0; i<nums.length; i+=2){
-            ans[i] = nums[remInd];
-            ans[i+1] = nums[remInd + n];
+            ans[i] = nums[remInd]; // adding i'th number to the ans array
+            ans[i+1] = nums[remInd + n]; // adding (remInd+n)'th number to the ans array
             remInd++;
         }
         return ans;
