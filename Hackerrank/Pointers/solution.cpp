6191d250275a6527d9e9b2e9e03b507095384a82
hitesh11tahiliani <80666992+hitesh11tahiliani@users.noreply.github.com>
2021-10-03 12:32:19 +0530

    comments added

--- Hackerrank/Pointers/solution.cpp
+++ Hackerrank/Pointers/solution.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+// The function is declared with a void return type, so there is no value to return. Modify the values in memory so that  contains their sum and  contains their absoluted difference.
+
+
 void update(int *a,int *b) {
+    
+    // a'= a + b 
     if (*a>*b){
         *b = *a - *b;
         *a = *a - *b + *a;
     }
+    // b' = |a - b|
     else {
         *b = *b - *a;
         *a = *a + *b + *a;
@@ -14,9 +20,11 @@ void update(int *a,int *b) {
 }
 
 int main() {
+    // initalising intengers
     int a, b;
     int *pa = &a, *pb = &b;
     
+    //printing the output
     scanf("%d %d", &a, &b);
     update(pa, pb);
     printf("%d\n%d", a, b);
