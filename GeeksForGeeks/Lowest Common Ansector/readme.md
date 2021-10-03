58648c8adbcf92a296b75c61c15cb65bcb295584
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-10-03 22:37:02 +0530

    Lowest common ansector

--- GeeksForGeeks/Lowest Common Ansector/readme.md
+++ GeeksForGeeks/Lowest Common Ansector/readme.md
@@ -1,3 +1,23 @@
 # Find Lowest Common Ancestor in a Given Binary Tree
+# Test cases :- 
+    (i)
+    n1 = 2 , n2 =  3
+        1
+    /  \
+    2    3
+    Output: 1
+    Explanation:
+    LCA of 2 and 3 is 1.
 
+    (ii)
+    n1 = 3 , n2 = 4
+         5
+        /
+       2
+     /  \
+    3    4
+    Output: 2
+    Explanation: 
+    LCA of 3 and 4 is 2. 
+    
 ## Question link :- https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1/?track=DSASP-Tree&batchId=154
--- GeeksForGeeks/Lowest Common Ansector/solution.cpp
+++ GeeksForGeeks/Lowest Common Ansector/solution.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Utility function creates a new binary tree node with given key
 struct Node
 {
     int key;
@@ -13,20 +14,25 @@ struct Node
 };
 Node *lca(Node *root, int n1, int n2)
 {
+    // base case
     if (root == NULL)
     {
         return NULL;
     }
+    // If either n1 or n2 matches with root's key, we return it (Note that if a key is
+    // ancestor of other, then the ancestor key becomes LCA.
     if (root->key == n1 || root->key == n2)
     {
         return root;
     }
+    // Look for keys in left and right subtrees
     Node *lca1 = lca(root->left, n1, n2);
     Node *lca2 = lca(root->right, n1, n2);
     if (lca1 != NULL && lca2 != NULL)
     {
         return root;
     }
+
     if (lca1 != NULL)
     {
         return lca1;
@@ -52,3 +58,4 @@ int main()
     cout << "LCA(2, 4) = " << lca(root, 2, 4)->key << endl;
     return 0;
 }
+// Time complexity is O(n)
