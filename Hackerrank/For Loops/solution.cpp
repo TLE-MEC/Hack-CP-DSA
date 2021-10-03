eb4f23e0c1a9feb8615b12c878f83776202eb807
Lokesh Rathore <89118388+Lokesh-Rathore@users.noreply.github.com>
2021-10-03 12:18:52 +0530

    comments added
    I added comments describing the solution code as asked

--- Hackerrank/For Loops/solution.cpp
+++ Hackerrank/For Loops/solution.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
-#include <cstdio>
+#include <iostream>  //header file
+#include <cstdio>    //header file
 using namespace std;
 
-int main() {
+int main() {          //starting main function
     
-    int a,b;
-    cin>>a>>b;
+    int a,b;          //declaring integer variables
+    cin>>a>>b;        //taking input from user
     
-    string one[] = {" ", "one","two","three","four","five","six","seven","eight","nine"};
+    string one[] = {" ", "one","two","three","four","five","six","seven","eight","nine"};       //declaring a string 
     
-    for(int i=a;i<=b;i++){
-        if(i<=9){
+    for(int i=a;i<=b;i++){                  //starting for loop
+        if(i<=9){                           //if value of i is somewhere between a and b then it'll print the index value of that from string one[].
             cout<<one[i]<<endl;
         
         }
-        else if (i%2==0){
+        else if (i%2==0){                   //if i is even i.e devisible by 2 then print "even".
             cout<<"even"<<endl;
         }
         else{
-            cout<<"odd"<<endl;
+            cout<<"odd"<<endl;              //else print odd
         }
     }
-    
-    
-
-    // Complete the code.
-    return 0;
+    return 0;                               
 }
