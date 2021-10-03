afd6f6423ed99857ebda855b204324278ecf23f5
Anmol Jain <72291893+anmol-19@users.noreply.github.com>
2021-10-03 15:13:55 +0530

    Update Target_sum.cpp
    I update my solution.cpp

--- Leetcode/Target_sum.cpp
+++ Leetcode/Target_sum.cpp
@@ -6,21 +6,26 @@ public:
         {
             sum=sum+nums[i];
         }
+        // check if target value is greater then sum of vector elements then answer will be zero always
+        // also check if sum-target is odd then answer will be zero always
         if(target>sum || (sum-target)%2!=0 )
         {
             return 0;
         }
-  
+        // if target is zero then answer will be zero
         if(sum%2!=0 && target==0)
         {
             return 0;
         }
         int nsum=(sum+target)/2;
+        // in case if we have negative target then we have to make it positive to find actual answer
         if(nsum<0)
         {
             nsum=-1*nsum;
         }
+        // creating 2d matrix to store answer of  repeative recursive call
         int a[nums.size()+1][nsum+1];
+        // intialising matrix with recursive function base value
         for(int i=0;i<(nums.size()+1);i++)
         {
             for(int j=0;j<nsum+1;j++)
@@ -35,6 +40,7 @@ public:
                 }
             }
         }
+        // start filling matrix using recursive method memorisation
         for(int i=1;i<(nums.size()+1);i++)
         {
             for(int j=0;j<nsum+1;j++)
@@ -49,6 +55,7 @@ public:
                 }
             }
         }
+        // return final answer
         return a[nums.size()][nsum];
     }
 };
