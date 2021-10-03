3026595db8aec6a44cd2ed1912c491fe52da4df7
Abinash <abinashsamal18@gmail.com>
2021-10-03 16:59:19 +0530

    Added No Time To Wait problem Codechef

---
+++ Codechef/No Time To Wait/solution.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,h,n;
+    cin>>n>>h>>x; /*Taking input of n=number of time zones chef can travel to,
+    h=number of hours needed for chef to solve that question,
+    and x=number of hours left with chef*/
+    int t[n];   //taking an array of size n
+    string f="yes"; //initiating a temporary variable x
+    for(int i=0;i<n;i++)
+        cin>>t[i];  //taking input of the timezones to which chef can jump
+    for(int i=0;i<n;i++)
+    {
+        if(t[i]+x>=h)   /*If  time left with chef +any one of the timezones to 
+        which he can jump is greater than the required time to solve the 
+        problem then he can jump to that time and solve the problem.Otherwise
+        he wouldnt be able to solve that problem*/
+        {
+            cout<<f;
+            exit(0);
+        }
+    }
+    f="no";
+    cout<<f;
+    return 0;
+}
