6ff20ff5bb6b942a9c6fd151dd72847191e57f8a
Alisha <alishakamat8@gmail.com>
2021-10-05 00:52:36 +0530

    Added solution.cpp

---
+++ Hackerrank/Designer PDF Viewer/solution.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include<string.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// The designerPdfViewer function 
+int designerPdfViewer(vector<int> h, string word) 
+{
+    int highest=0,area=0,pos=0;
+    int l=word.length();
+    for(int i=0;i<l;i++)
+    {
+        pos=(word[i]-97);    //To get individual character heights
+        if(highest<h[pos])
+        highest=h[pos];     //Considering only the highest height among all others
+    }
+    area=highest*l;         //FInding the area covered by the alphabet sequence
+    return area;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
