960d0d2569941a93604b89b4cfee1f202f902d3e
Ayush Pawar <54628130+Spyy004@users.noreply.github.com>
2021-10-04 11:00:06 +0530

    Update Code.cpp

--- Leetcode/Buy And Sell Stock IV/Code.cpp
+++ Leetcode/Buy And Sell Stock IV/Code.cpp
@@ -4,26 +4,26 @@ public:
     {
         if(curr==prices.size() || k==k2)
             return 0;
-        string key = to_string(curr) + "-" + to_string(isBuy) + "-" + to_string(k);
+        string key = to_string(curr) + "-" + to_string(isBuy) + "-" + to_string(k);   // our memo. memo will hold the max profit for a currentIndex/bought or not bought/transaction number
        if( m.find(key)!=m.end())
            return m[key];
         int x1,x2;
-            if(!isBuy)
+            if(!isBuy)   // if we haven't bought anything, we can buy stocks today. so we have 2 choices. buy or dont buy
             {
                 x1 = solve(prices,!isBuy,k,curr+1,k2,m)-prices[curr];
                 x2 = solve(prices,isBuy,k,curr+1,k2,m);
             }
-            else
+            else  // else we have 2 choices. sell today or dont sell
             {
                 x1 = solve(prices,!isBuy,k+1,curr+1,k2,m)+prices[curr];
                 x2 = solve(prices,isBuy,k,curr+1,k2,m);
             }
         
-        return m[key]= max(x1,x2);
+        return m[key]= max(x1,x2);  // the best solution will be the maximum of 2 cases
     }
     int maxProfit(int k, vector<int>& prices) {
-        bool isBuy=false;
-        map<string,int>m;
+        bool isBuy=false;  // nothing is bought currently
+        map<string,int>m;   
         return solve(prices,isBuy,0,0,k,m);
     }
 };
