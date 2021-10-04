e135424e873d59dfb1114a88465ff07ffa877506
Ambareen Azam <64471630+Ambareen09@users.noreply.github.com>
2021-10-04 15:32:35 +0530

    Create Solution.cpp

---
+++ GeeksForGeeks/Josephus problem/Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int josephus(int n, int k)
+{
+	if (n == 1)
+		return 1;
+	else
+		return (josephus(n - 1, k) + k - 1) % n + 1;
+}
+int main()
+{
+	int n = 14;
+	int k = 2;
+	cout << "The chosen place is " << josephus(n, k);
+	return 0;
+}
+
