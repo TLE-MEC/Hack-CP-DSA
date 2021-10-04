1332517f782401a91f3abb393f0b02657f1b0c09
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-10-04 08:32:35 +0530

    Added comments

--- Hackerrank/Zig Zag Sequence/solution.cpp
+++ Hackerrank/Zig Zag Sequence/solution.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 void findZigZagSequence(vector < int > a, int n){
+    
+    // sorting the vector
     sort(a.begin(), a.end());
+    
+    // finding mid and swaping it with last element
     int mid = (n - 1)/2;
     swap(a[mid], a[n-1]);
 
+    // moving start index forward and end index backwards
     int st = mid + 1;
     int ed = n - 2;
     while(st <= ed){
@@ -13,6 +18,8 @@ void findZigZagSequence(vector < int > a, int n){
         st = st + 1;
         ed = ed - 1;
     }
+    
+    // priting the vector
     for(int i = 0; i < n; i++){
         if(i > 0) cout << " ";
         cout << a[i];
@@ -24,7 +31,7 @@ int main() {
     int n, x;
     int test_cases;
     cin >> test_cases;
-
+    // driver code
     for(int cs = 1; cs <= test_cases; cs++){
         cin >> n;
         vector < int > a;
