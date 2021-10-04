354a3c2dd59946782fff4dd1fc7c83d1522ba80a
abelzach <abelsimonz@gmail.com>
2021-10-04 22:45:21 +0530

    Added new problem

---
+++ Hackerrank/New Year Chaos/readme.md
@@ -0,0 +1,1 @@
+https://www.hackerrank.com/challenges/new-year-chaos/problem
---
+++ Hackerrank/New Year Chaos/solution.cpp
@@ -0,0 +1,115 @@
+/* Each person wears a sticker indicating their initial position in the queue from 1 to n. Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic. */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumBribes' function below.
+ *
+ * The function accepts INTEGER_ARRAY q as parameter.
+ */
+
+void minimumBribes(vector<int> q) {
+    int n=q.size();
+    int l=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(q[i]!=(i+1))
+        {
+            if((i-1)>=0&&q[i-1]==(i+1)){
+                l++;
+                swap(q[i],q[i-1]);
+            }
+            else if((i-2)>=0&&q[i-2]==(i+1)){
+                l=l+2;
+                q[i-2]=q[i-1];
+                q[i-1]=q[i];
+                q[i]=i+1;
+            }
+            else{
+                printf("Too chaotic\n");
+                return;
+            }
+        }      
+    }
+    printf("%d\n",l);
+    return;
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split(rtrim(q_temp_temp));
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
