e549d6637a92d27b838af0da30363d19595c6262
Arihantkapoor <62657676+Arihantkapoor@users.noreply.github.com>
2021-10-04 16:58:41 +0530

    added solution to SUPCHEF codechef problem

---
+++ Codechef/The Preparations/Solution.cpp
@@ -0,0 +1,22 @@
+// Problem Code: SUPCHEF
+// Difficulty level: Easy
+// Cpp implementation
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int m,n,k;
+	    cin>>m>>n>>k;
+	    if(n*k<m){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
