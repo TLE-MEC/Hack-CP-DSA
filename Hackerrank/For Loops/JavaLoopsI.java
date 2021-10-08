751fdca948ba8a1d8d5874108fd04f92b49580f1
Ujjwal Dhakal <68050149+ujjwalnp@users.noreply.github.com>
2021-10-08 12:36:18 +0545

    Create JavaLoopsI.java
    This is the solution of problem Java Loops I which is in hackerrank.
    https://www.hackerrank.com/challenges/java-loops-i/problem

---
+++ Hackerrank/For Loops/JavaLoopsI.java
@@ -0,0 +1,11 @@
+class JavaLoopsI {
+    public static void main(String args[])
+    {
+        
+        // for loop begins when i=1
+        // and runs till i <=10
+        // i is incremented by 1 in every iteration
+        for (int i = 1; i <= 10; i++)
+            System.out.println(i);
+    }
+}
