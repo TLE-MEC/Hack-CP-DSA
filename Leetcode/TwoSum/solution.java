0f48754f49100ef5284876d1e73ec338d8b9b06b
Suvam Routray <72883939+RayOPnub@users.noreply.github.com>
2021-10-26 23:06:06 +0530

    Update solution.java

--- Leetcode/TwoSum/solution.java
+++ Leetcode/TwoSum/solution.java
@@ -1,18 +1,25 @@
-class Solution {
+class Solution 
+{
         public int[] twoSum(int[] nums, int target) 
         {
             int c;
+            //for loop to check every element in the array
             for (int i = 0; i<nums.length; i++) 
             {
                 c = target - nums[i];
+                //loop to find complement
                 for (int j = 0; j<nums.length; j++) 
                 {
-                    if (i ==  j) { continue; }
-                    if (nums[j] == c) {
-                        return new int[] {i, j};
+                    if (i ==  j) 
+                    { 
+                           continue; 
+                    }
+                    if (nums[j] == c) 
+                    {
+                    return new int[] {i, j};
                     }
                 }
             }
             return new int[] {0, 0};
         }
-    }
+}
